2bf8db63c23b5df9facc4e74ca0893a801494961
ftourn <ftourn@ndway.com>
2020-11-01 12:48:54 -0300

    Se largan los listeners cuando el fragment de sensor está en pausa

--- app/src/main/java/com/example/soa2020ea3/SensorsFragment.java
+++ app/src/main/java/com/example/soa2020ea3/SensorsFragment.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Lifecycle;
 
 import android.service.autofill.TextValueSanitizer;
 import android.util.Log;
@@ -49,17 +50,7 @@ public class SensorsFragment extends Fragment implements SensorEventListener {
     private TextView lumenValue;
     private Button btn_luz;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public SensorsFragment() {
-
     }
 
     @Override
@@ -68,24 +59,6 @@ public class SensorsFragment extends Fragment implements SensorEventListener {
         if (context instanceof Activity) {
             mActivity = (Activity) context;
         }
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SensorsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SensorsFragment newInstance(String param1, String param2) {
-        SensorsFragment fragment = new SensorsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
@@ -98,11 +71,25 @@ public class SensorsFragment extends Fragment implements SensorEventListener {
 
         mSensorManager.registerListener(this, mSensorAcelerometro, SensorManager.SENSOR_DELAY_NORMAL);
         mSensorManager.registerListener(this, mSensorLuz, SensorManager.SENSOR_DELAY_NORMAL);
+    }
 
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    @NonNull
+    @Override
+    public Lifecycle getLifecycle() {
+        return super.getLifecycle();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mSensorAcelerometro, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mSensorLuz, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
     }
 
     @Override
